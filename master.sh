2697f54991c9a652e43e0248a0ca29d923e0dc97
Jorge Romero <jeamaya1991@gmail.com>
2024-03-18 18:00:52 -0400

    Updated wdl

--- master.sh
+++ master.sh
@@ -57,8 +57,12 @@ mv Data/pf3d7_ref_updated_v4_ref1.fasta reference_panel_1.fasta
 mv Data/pfdd2_ref_updated_v3_ref2.fasta reference_panel_2.fasta
 mv Data/snv_filters.txt .
 
-python Code/Amplicon_TerraPipeline.py --config config.json --terra --meta --adaptor_removal --contamination --separate_reads --primer_removal --dada2 --postproc_dada2 --asv_to_cigar
+#For SIMPLseq
+#python Code/Amplicon_TerraPipeline.py --config config.json --terra --meta --adaptor_removal --contamination --separate_reads --primer_removal --dada2 --postproc_dada2 --asv_to_cigar
 #Rscript /render_report.R -d "/Report/Merge/" -o "/Report/" -p "/barcodes_matches.csv" -m 1000 -c 0.5 -mf "/Results/missing_files.tsv"
+
+#For Guyana Run
+python Code/Amplicon_TerraPipeline.py --config config.json --terra --meta --adaptor_removal --separate_reads --primer_removal --dada2 --postproc_dada2 --asv_to_cigar
 
 #Tested
 #python Code/Amplicon_TerraPipeline.py --config config_iSeq.json --mixed_reads \
